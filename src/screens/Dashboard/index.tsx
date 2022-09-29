cc3eaee6e7fe5c0c7cb7fd831d0e078dd65ce743
Naua-stack <nauafernandesvv@gmail.com>
2022-09-28 22:01:46 -0300

    feat: add logout button as borderless button

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -17,6 +17,7 @@ import {
   Transactions,
   Title,
   TransactionsList,
+  LogoutButton,
 } from "./styles";
 
 export interface DataListProps extends TransactionCardProps {
@@ -77,7 +78,9 @@ export function Dashboard() {
             </User>
           </UserInfo>
 
-          <Icon name="power" />
+          <LogoutButton onPress={() => {}}>
+            <Icon name="power" />
+          </LogoutButton>
         </UserWrapper>
       </Header>
 
