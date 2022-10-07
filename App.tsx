d1f3e8dc0a5d7e07084aeb52fed4e727030d5908
Naua-stack <nauafernandesvv@gmail.com>
2022-10-06 21:55:20 -0300

    feat: add status bar light content

--- App.tsx
+++ App.tsx
@@ -14,6 +14,7 @@ import {
 import theme from "./src/global/styles/theme";
 import { NavigationContainer } from "@react-navigation/native";
 import { AppRoutes } from "./src/routes/app.routes";
+import { StatusBar } from "react-native";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -29,6 +30,7 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <NavigationContainer>
+        <StatusBar barStyle="light-content" />
         <AppRoutes />
       </NavigationContainer>
     </ThemeProvider>
